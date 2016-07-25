31a0788c361261f6b89e4e878e3c9b8c142172eb
chenyuan <cy476571@gmail.com>
2016-07-25 18:04:45 +0800

    add  country data and  timezone data to TInfo component.

--- app/main.jsx
+++ app/main.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //import Test from './components/Test.jsx';
 import SiteHeader from './components/SiteHeader';
 import SiteFooter from './components/SiteFooter';
@@ -19,24 +18,22 @@ import ActivateEmail from './teacher-components/ActivateEmail';
 import TInfo from './teacher-components/TInfo';
 import FacebookLogin from 'react-facebook-login';
 
+// tap event plugin initialization.
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-  }
+    }
 
   render () {
     return (
       <div className="weteach">
         <HomepageHeader></HomepageHeader>
-        <FacebookLogin
-        appId="267768116929978"
-        autoLoad={true}
-        fields="name,email,picture"
-        ></FacebookLogin>
-        <MuiThemeProvider>
-          <TInfo></TInfo>
-        </MuiThemeProvider>
+        {/*<FacebookLogin appId="267768116929978" autoLoad={true} fields="name,email,picture"></FacebookLogin>*/}
+        <TInfo></TInfo>
         <SiteFooter></SiteFooter>
         <ul id="beian">
           <li> &copy;&nbsp;WeTeach</li>
--- app/teacher-components/TInfo.jsx
+++ app/teacher-components/TInfo.jsx
@@ -1,21 +1,27 @@
 // complete teacher info.
 
 import React from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import DatePicker from 'material-ui/DatePicker';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
+import {Typeahead} from 'react-typeahead';
+import reqwest from 'reqwest';
 
 class TInfo extends React.Component {
 
   constructor (props) {
     super (props);
     this.state = {
-      value: 1
+      value: null,
+      clicked: false,
+      countriesList: [],
+      timezoneList: []
     };
   }
 
   handleChange (e, index, value) {
+    this.setState({ clicked: true });
     this.setState({value});
   }
 
@@ -28,23 +34,73 @@ class TInfo extends React.Component {
       width: "100%"
     };
 
+    const countriesList = this.state.countriesList.map((country) => {
+      return country.cname;
+    });
+
     return (
-      <div className="t-info">
-        <h1 className="t-info-caption text-center">Please complete your personal information</h1>
-        <form action="/t-info" className="t-info-form">
-          <SelectField value={this.state.value} onChange={this.handleChange} floatingLabelText="Select Your Country">
-            <MenuItem value={1} primaryText="Australia" />
-            <MenuItem value={2} primaryText="America" />
-            <MenuItem value={3} primaryText="England" />
-            <MenuItem value={4} primaryText="Canada" />
-            <MenuItem value={5} primaryText="other countries" />
-          </SelectField>
-          <br/>
-          <FlatButton type="submit" label="Submit" primary={true} onClick={this.handleSubmit} style={style}></FlatButton>
-        </form>
-      </div>
+      <MuiThemeProvider>
+        <div className="t-info">
+          <h1 className="t-info-caption text-center">Please complete your personal information</h1>
+          <form action="/t-info" className="t-info-form">
+            <Typeahead options={countriesList} maxVisible={5} placeholder="Your country"></Typeahead>
+            <br/>
+            <DatePicker hintText="Your Birth" autoOk={true} cancelLabel="取消"></DatePicker>
+            <br/>
+            <br/>
+            <FlatButton type="submit" label="Submit" primary={true} onClick={this.handleSubmit.bind(this)} style={style}></FlatButton>
+          </form>
+        </div>
+      </MuiThemeProvider>
     )
   }
+
+  componentDidMount () {
+    var self = this;
+
+    var countryRequest =  reqwest({
+      url: "http://api.weteach.test/v1/loc/country",
+      method: "get"
+    })
+    .then((resp) => {
+      console.log(resp);
+      if (resp.success) {
+        self.setState({
+          countriesList: resp.data
+        });
+      }
+    })
+    .fail((err,msg) => {
+      console.log(err);
+      console.log("数据请求错误");
+    });
+
+    console.log(countryRequest);
+
+    var timezoneRequest = reqwest({
+      url: "http://api.weteach.test/v1/loc/timezone",
+      method: "get"
+    })
+    .then((resp) => {
+      console.log(resp);
+      if (resp.success) {
+        self.setState({
+          timezoneList: resp.data
+        });
+      }
+    })
+    .fail((err, msg) => {
+      console.log(err);
+      console.log("timezone data request error.");
+    });
+  }
+
+  componentWillUnmount () {
+    countryRequest.abort();
+    timezoneRequest.abort();
+  }
+
+  // cityList  data  request.
 }
 
 export default TInfo;
