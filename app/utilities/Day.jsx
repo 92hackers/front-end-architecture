8447e92a0f95ba8be863d4004bd9d9f3bd7500f5
chenyuan <cy476571@gmail.com>
2016-08-04 17:39:04 +0800

    add  day component data  rendering function.

--- app/utilities/Day.jsx
+++ app/utilities/Day.jsx
@@ -9,9 +9,10 @@ class Day extends React.Component {
 
   render () {
 
-    const times = ["12 AM", "", "1 AM", "", "2 AM", "", "3 AM", "", "4 AM", "", "5 AM", "", "6 AM", "", "7 AM", "",
-      "8 AM", "", "9 AM", "", "10 AM", "", "11 AM", "", "noon", "", "1 PM", "", "2 PM", "",  "3 PM", "",  "4 PM", "",
-      "5 PM", "", "6 PM", "", "7 PM", "", "8 PM", "", "9 PM", "", "10 PM", "", "11 PM", "", "12 AM"
+    const times = [
+      "12 AM", "12:30 AM", "1 AM", "1:30 AM", "2 AM", "2:30 AM", "3 AM", "3:30 AM", "4 AM", "4:30 AM", "5 AM", "5:30 AM", "6 AM", "6:30 AM", "7 AM", "7:30 AM",
+      "8 AM", "8:30 AM", "9 AM", "9:30 AM", "10 AM", "10:30 AM", "11 AM", "11:30 AM", "12:00", "12:30", "1 PM", "1:30 PM", "2 PM", "2:30 PM", "3 PM", "3:30 PM", "4 PM", "4:30 PM",
+      "5 PM", "5:30 PM", "6 PM", "6:30 PM", "7 PM", "7:30 PM", "8 PM", "8:30 PM", "9 PM", "9:30 PM", "10 PM", "10:30 PM", "11 PM", "11:30 PM", "12 AM"
     ];
 
     const months = ["January", "February", "March", "April", "May", "June",
@@ -44,10 +45,17 @@ class Day extends React.Component {
         <ul id="day-time-box">
         {
           times.map((time, index) => {
+            let data = "";
+            let regExp = /:30/;
+            if (regExp.test(time)) {
+              data = "";
+            } else {
+              data = time;
+            }
             return (
               <li key={index} className="time-point row">
-                <div className="time-tag col-3"><span className="inner-time-value">{time}</span></div>
-                <div className="time-line col-9"></div>
+                <div className="time-tag col-3"><span className="inner-time-value">{data}</span></div>
+                <div data-time={time} className="time-line col-9"></div>
               </li>
             )
           })
@@ -61,6 +69,27 @@ class Day extends React.Component {
     var self = this;
     var targetBox = document.getElementById("day-time-box");
     targetBox.scrollTop =  17.5 * 100;
+
+    var timeData = ["10:30 PM", "6:30 PM", "1:30 PM", "4:30 AM"];
+
+    //TODO:  fetch data from server.
+
+    var timeLineElems = document.getElementsByClassName("time-line");
+
+    var count = 0;
+
+    for (let i = 0; i < timeLineElems.length; i++) {
+      for (let j = 0; j < timeData.length; j++) {
+        if (timeLineElems[i].dataset.time === timeData[j]){
+          count++;
+          timeLineElems[i].style.backgroundColor = "#ddd";
+          break;
+        }
+      }
+      if (count === timeData.length) {
+        break;
+      }
+    }
   }
 
 }
--- app/utilities/Week.jsx
+++ app/utilities/Week.jsx
@@ -8,7 +8,7 @@ class Week extends React.Component {
     super (props);
     this.timesData = [
       "12 AM", "12:30 AM", "1 AM", "1:30 AM", "2 AM", "2:30 AM", "3 AM", "3:30 AM", "4 AM", "4:30 AM", "5 AM", "5:30 AM", "6 AM", "6:30 AM", "7 AM", "7:30 AM",
-      "8 AM", "8:30 AM", "9 AM", "9:30 AM", "10 AM", "10:30 AM", "11 AM", "11:30 AM", "12:00 AM", "12:30 AM", "1 PM", "1:30 PM", "2 PM", "2:30 PM", "3 PM", "3:30 PM", "4 PM", "4:30 PM",
+      "8 AM", "8:30 AM", "9 AM", "9:30 AM", "10 AM", "10:30 AM", "11 AM", "11:30 AM", "12:00", "12:30", "1 PM", "1:30 PM", "2 PM", "2:30 PM", "3 PM", "3:30 PM", "4 PM", "4:30 PM",
       "5 PM", "5:30 PM", "6 PM", "6:30 PM", "7 PM", "7:30 PM", "8 PM", "8:30 PM", "9 PM", "9:30 PM", "10 PM", "10:30 PM", "11 PM", "11:30 PM", "12 AM"
     ];
     this.weekDays = [];
