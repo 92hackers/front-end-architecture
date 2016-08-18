96fe27c474a8741ed753c634ff39d37943649240
chenyuan <cy476571@gmail.com>
2016-08-18 15:25:56 +0800

    add description on t about school section 2, and fixed some mistakes.

--- app/teacher-components/TAboutSchool.jsx
+++ app/teacher-components/TAboutSchool.jsx
@@ -22,31 +22,48 @@ class TAboutSchool extends React.Component {
           <section className="wish">
             <h1 className="title">Why Are We Building WeTeach</h1>
             <ul className="icons">
-              <li className="icon icon-plant-tree"></li>
-              <li className="icon icon-drink"></li>
-              <li className="icon icon-love"></li>
-              <li className="icon icon-connect"></li>
+              <li className="icon icon-plant-tree">
+                <div className="mask">
+                  <p className="content">We want to inspire our students to be the best people they can be through quality, fun, and caring online education, and get parents involved in this process.</p>
+                </div>
+              </li>
+              <li className="icon icon-drink">
+                <div className="mask">
+                  <p className="content">We want to create fulfilling, flexible, and financially rewarding opportunities for teachers to teach from the comfort of their own home.</p>
+                </div>
+              </li>
+              <li className="icon icon-love">
+                <div className="mask">
+                  <p className="content">We want education to be personal and customized, not transactional, where teachers, students, and parents forge lifelong friendships.</p>
+                </div>
+              </li>
+              <li className="icon icon-connect">
+                <div className="mask">
+                  <p className="content">We want to bring people from all over the world together into a community where they can learn about each other's language, culture and history.</p>
+                </div>
+              </li>
             </ul>
           </section>
           <section className="community">
             <h1 className="title">Online Community</h1>
             <p>We foster an online community where teachers, parents, </p>
-            <p>and students can share with and learn from each other.</p>
+            <p className="last-p">and students can share with and learn from each other.</p>
             <div className="icon icon-circle"></div>
           </section>
           <section className="charity">
-            <h1 className="title">Support the Society</h1>
+            <h1 className="title">Support Society</h1>
             <h2 className="sub-title">Part of our proceeds goes to charities supporting rural education in China.</h2>
             <div className="icon-charity"></div>
           </section>
           <section className="students-video">
             <h1 className="title">Students' Video</h1>
+            <p className="last-p">Felix saying thanks for all the help heather has given him</p>
             <iframe width="1024" height="576" src="https://www.youtube.com/embed/-2wJlQ-uxl0" frameBorder="0" allowFullScreen></iframe>
           </section>
           <section className="activities">
             <h1 className="title">Offline Activities</h1>
             <p>We organise study tours to the Anglosphere so students can learn more about Western culture </p>
-            <p>and meet their teachers in person. We also arrange for teachers to visit their students in China.</p>
+            <p className="last-p">and meet their teachers in person. We also arrange for teachers to visit their students in China.</p>
             <ul className="icons clearfix">
               <li className="icon-one left"></li>
               <li className="icon-two left"></li>
@@ -56,7 +73,7 @@ class TAboutSchool extends React.Component {
             </ul>
           </section>
           <section className="slogan">
-            <p className="slogan-title">Felix saying thanks for all the help heather has given him.</p>
+            <p className="slogan-title">"Learning for life. Friendships for life."</p>
           </section>
         </section>
       </section>
